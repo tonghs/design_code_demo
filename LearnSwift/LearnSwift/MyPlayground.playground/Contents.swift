1a179005444c9e5b516bfcc414456119b04fea4c
tonghs <tonghuashuai@gmail.com>
2021-03-19 23:29:38 +0800

    fix

--- LearnSwift/LearnSwift/MyPlayground.playground/Contents.swift
+++ LearnSwift/LearnSwift/MyPlayground.playground/Contents.swift
@@ -226,3 +226,353 @@ print(newArry2)
 let newArry3 = numbers.sorted { $0 < $1 }
 
 print(newArry3)
+
+
+class Shape {
+    var numberOfSides = 0
+    
+    func simpleDesc() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+
+
+var shape = Shape()
+shape.numberOfSides = 7
+
+var shapeDesc = shape.simpleDesc()
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDesc() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+
+var namedShape = NamedShape(name: "testShape")
+
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDesc() -> String {
+        return "A Square with sides of length \(sideLength)"
+    }
+}
+
+let square = Square(sideLength: 2, name: "square")
+square.area()
+square.simpleDesc()
+
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3 * self.sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3, name: "triangle")
+
+triangle.perimeter
+triangle.perimeter = 9.9
+triangle.sideLength
+triangle.simpleDesc()
+
+
+var optionalSquare: Square? = Square(sideLength: 4, name: "optional square")
+
+var sideLength = optionalSquare?.sideLength
+
+optionalSquare = nil
+
+optionalSquare?.sideLength
+
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(sideLength: Double, name: String) {
+        square = Square(sideLength: sideLength, name: name)
+        triangle = EquilateralTriangle(sideLength: sideLength, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(sideLength: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDesc() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .two:
+            return "two"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+ace.simpleDesc()
+
+let three = Rank.three
+three.simpleDesc()
+three.rawValue
+
+let king = Rank.king
+
+king.simpleDesc()
+king.rawValue
+
+if let newRank = Rank(rawValue: 2) {
+    _ = newRank.simpleDesc()
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+
+protocol ExampleProtocol {
+    var simpleDesc: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDesc: String = "A very simple class."
+    var anotherProperty: Int = 500
+    
+    func adjust() {
+        simpleDesc += " Now 100% adjusted"
+    }
+}
+
+var a = SimpleClass()
+
+a.adjust()
+
+let aDesc = a.simpleDesc
+
+
+
+struct SimpleStructure: ExampleProtocol {
+    
+    var simpleDesc: String = "A simple structure"
+    
+    mutating func adjust() {
+        simpleDesc += " (adjusted)"
+    }
+}
+
+
+var b = SimpleStructure()
+b.adjust()
+b.simpleDesc
+
+
+
+extension Int: ExampleProtocol {
+    var simpleDesc: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+
+print(9.simpleDesc)
+
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never has toner" {
+        throw PrinterError.noToner
+    }
+    
+    return "Job Sent"
+}
+
+do {
+    let printerResponse = try send(job: 400, toPrinter: "Bi Sheng")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try send(job: 400, toPrinter: "Never has toner")
+    print(printerResponse)
+} catch PrinterError.noToner {
+    print("no toner")
+} catch PrinterError.onFire {
+    print("onFire")
+} catch {
+    print(error)
+}
+
+
+let printerSuccess = try? send(job: 300, toPrinter: "aaa")
+let printerFailure = try? send(job: 500, toPrinter: "Never has toner")
+
+
+var fridgeIsOpen = false
+let fridgeContent: [String] = ["milk", "egg", "leftovers"]
+
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    
+    defer {
+        fridgeIsOpen = false
+    }
+    
+    return fridgeContent.contains(food)
+}
+
+print(fridgeIsOpen)
+fridgeContains("banana")
+print(fridgeIsOpen)
+
+var result = [Int]()
+
+
+
+func makeArry<Item>(repeating item: Item, times: Int) -> [Item] {
+    var result = [Item]()
+    
+    for _ in 0..<times {
+        result.append(item)
+    }
+    
+    return result
+}
+
+makeArry(repeating: "a", times: 5)
+
+
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool where T.Element: Equatable, T.Element == U.Element {
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+anyCommonElements([1, 2, 3], [4])
+anyCommonElements([1, 2, 3], [3, 4])
+
+
+func makeInt() -> some Equatable {
+    Int.random(in: 1...10)
+}
+
+func makeStr() -> some Equatable {
+    return "aaa"
+}
+
+let int1 = makeInt()
+let int2 = makeInt()
+
+int1
+int2
+
+int1 == int2
+
+let ssss = makeStr()
+
+
+func doSth(str: String?) -> String {
+    guard let v = str, str != "" else {
+        return "not ok"
+    }
+
+    return "ok" + v
+}
+
+doSth(str: nil)
+doSth(str: "")
+doSth(str: "tonghs")
