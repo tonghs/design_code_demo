b69047ce4de6835eda77d70fb57b2c73a4a7ca37
tonghs <tonghuashuai@gmail.com>
2021-04-08 11:56:40 +0800

    home screen quick action demo

---
+++ HomeScreenQuickActions/HomeScreenQuickActions/ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var quickActions: QuickActionService
+    
+    var body: some View {
+        VStack {
+            Text("Home Screen Quick Actions")
+            if let quickAction = quickActions.action {
+                Text("Selected Quick Action: \(quickAction.rawValue)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .environmentObject(QuickActionService())
+            ContentView()
+                .environmentObject(QuickActionService(initialValue: .newMessage))
+        }
+        
+    }
+}
---
+++ HomeScreenQuickActions/HomeScreenQuickActions/QuickAction.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+enum QuickAction: String {
+    case newMessage, search, inbox
+}
+
+final class QuickActionService: ObservableObject {
+    @Published var action: QuickAction?
+    
+    init(initialValue: QuickAction? = nil) {
+        action = initialValue
+    }
+}
