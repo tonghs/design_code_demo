a58961cfda6d5f52e1c92bfa099615601f441b84
tonghs <tonghuashuai@gmail.com>
2022-09-21 22:53:06 +0800

    test

---
+++ Focus/Focus/App/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by 仝华帅 on 2022/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
